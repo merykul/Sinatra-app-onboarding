bdf5e185ec8f86f024309ec8ac93ca44296719b7
Mariia Kulyk <mariakulv@gmail.com>
2023-07-26 16:07:48 +0300

    added get records method and post to database

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ end
 # RESTful Records Controllers Actions
 # index
 get '/people_list' do
-  @records = People.all
+  @records = Records.all
   erb(:"records/index")
 end
 
@@ -32,23 +32,45 @@ post '/create_person' do
   date_of_birth = params[:date_of_birth]
 
   if first_name.empty? || second_name.empty? || city.empty?
-    return "Fill in all required fields (first and second name, city)"
+    puts "Fill in all required fields (first and second name, city)"
   end
+
+  record = Records.new(
+    first_name: first_name,
+    second_name: second_name,
+    city: city,
+    date_of_birth: date_of_birth
+  )
 
   birth_date_message = date_of_birth.empty? ? "Date of birth is unknown" : "Date of birth: #{date_of_birth}"
 
-  "Person is added to the records! Full name: #{first_name} #{second_name}, City: #{city}, #{birth_date_message}"
+  if record.save
+    "Person is added to the records! Full name: #{first_name} #{second_name}, City: #{city}, #{birth_date_message}"
+    redirect('/people_list')
+  else
+    'Failed to save the record'
+  end
 end
 
 get '/added_person_form' do
   erb :create_person_form
 end
 
-put '/edit_persons_data' do
+# update
+put '/records/:id' do
+  @record = Records.find(params[:id])
+  # TODO
+  erb(:"records/edit")
   'Edit person page'
 end
 
-delete '/delete_person_record' do
+delete '/records/:id/delete' do
+  @record = Records.find(params[:id])
+  if @record.destroy
+    redirect('/added_person_form')
+  else
+    redirect("/records/#{@record.id}")
+  end
   'Delete record page'
 end
 
