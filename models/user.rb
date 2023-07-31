67710bb7d3a4aff0de41819ba018895bd20bb476
Mariia Kulyk <mariakulv@gmail.com>
2023-07-31 12:34:32 +0300

    fixed 'unknown user.password...' in User.create error

--- models/user.rb
+++ models/user.rb
@@ -1,15 +1,14 @@
 class User < ActiveRecord::Base
-  # TODO
-  # has_many: records
+  has_secure_password
   validates :username, presence: true, uniqueness: true, length: { within: 4..20, message: 'username length should be 4-20 symbols' }
   validates :first_name, presence: true
   validates :second_name, presence: true
-  validates :password, presence: true, uniqueness: true, length: { within: 6..15, message: 'password must have length from 6 to 15 sym.' }
-  validates :password_validation
+  validates :password, presence: true, length: { within: 6..15, message: 'password must have length from 6 to 15 sym.' }
+  # validates :password_validation
 
-  def password_validation
-    unless password.match?(/\A(?=.*[a-zA-Z])(?=.*\d)(?=.*\W)/)
-      errors.add(:password, "must include at least one letter, one digit, and one symbol")
-    end
-  end
+  # def password_validation
+  #   unless password.match?(/\A(?=.*[a-zA-Z])(?=.*\d)(?=.*\W)/)
+  #     errors.add(:password, "must include at least one letter, one digit, and one symbol")
+  #   end
+  # end
 end
