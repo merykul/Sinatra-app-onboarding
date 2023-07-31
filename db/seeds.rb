c098264af4ef396cd5c013aff46cacb3a0347964
Mariia Kulyk <mariakulv@gmail.com>
2023-07-31 12:26:45 +0300

    added generator for test data in records table

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ require_relative '../models/user.rb'
 puts "🌱 Seeding data..."
 puts "🌱 Generating TestUser for you..."
 
-User.create!(password: 'test123456', username: 'TestUser', first_name: 'Mariia', second_name: 'Tester')
+User.create!(password: 'Test123456!', username: 'TestUser', first_name: 'Mariia', second_name: 'Tester')
 
 puts"🌱 Generating random users..."
 
@@ -14,6 +14,15 @@ puts"🌱 Generating random users..."
   first_name = Faker::Name.unique.name
   second_name = Faker::Name.last_name
   User.create(password: password, username: username, first_name: first_name, second_name: second_name)
+end
+
+puts"🌱 Generating random Records..."
+10.times do
+  first_name = Faker::Name.unique.name
+  second_name = Faker::Name.last_name
+  city = Faker::Address.city
+  date_of_birth = Faker::Date.birthday
+  Records.create(first_name: first_name, second_name: second_name, city: city, date_of_birth: date_of_birth)
 end
 
 puts "✅ Done"
