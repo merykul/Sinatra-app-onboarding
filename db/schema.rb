f37dea65f6d94c7397ca0730dbee0de4e03345ee
Mariia Kulyk <mariakulv@gmail.com>
2023-08-08 12:23:11 +0300

    generated foreign key > user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_07_160833) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_08_090009) do
   create_table "cities", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "city"
     t.integer "number_of_records", default: 0
@@ -35,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_07_160833) do
     t.integer "role", default: 0
   end
 
+  add_foreign_key "records", "users"
 end
