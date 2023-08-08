15d992c050a33a887bd8a3535cdfab5e25066171
Mariia Kulyk <mariakulv@gmail.com>
2023-08-08 17:19:24 +0300

    added access handler for user management

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,39 @@ require_relative './application_controller'
 
 class UsersController < ApplicationController
 
+  get '/create_user' do
+    if current_user.role == 'admin'
+      erb :'user/create'
+    else
+      erb :'errors/users_profiles_access_error'
+    end
+  end
+
+  post '/create_user_form' do
+
+  end
+
+  get '/user/:id/edit' do
+    if current_user.role == 'admin'
+      erb :'user/edit'
+    else
+      erb :'errors/users_profiles_access_error'
+    end
+  end
+
+  patch '/user/:id/edit' do
+
+  end
+
+  get '/user/:id/delete' do
+    if current_user.role == 'admin'
+      erb :'user/delete'
+    else
+      erb :'errors/users_profiles_access_error'
+    end
+  end
+
+  delete '/user/:id/delete' do
+
+  end
 end
