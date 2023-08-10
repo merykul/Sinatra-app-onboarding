c7484c076e5246c317d6e17e737e28c5ef7c3204
Mariia Kulyk <mariakulv@gmail.com>
2023-08-10 17:05:55 +0300

    modified update_user helper method

--- app/controllers/controller_helper.rb
+++ app/controllers/controller_helper.rb
@@ -1,6 +1,20 @@
 module ControllerHelper
-  def update_user(user, parameters)
+  def update_user(user, if_error_erb, parameters, if_success_erb)
+    password_status = user.password_status
+    p "Password status: #{password_status}"
+
     user.update(parameters)
+
+    if user.valid?
+      user.save
+      p 'User is updated successfully!'
+      p "Password status: #{user.password_status}"
+      p "Username: #{user.username}"
+      redirect to if_success_erb
+    else
+      @error_messages = @user.errors.full_messages
+      erb if_error_erb
+    end
   end
 
   private
