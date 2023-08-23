3688719e6677b062d1f92e6cb6845976bfce3030
Mariia Kulyk <mariakulv@gmail.com>
2023-08-23 16:52:20 +0300

    added some more log methods

--- spec/helpers/logs_helper.rb
+++ spec/helpers/logs_helper.rb
@@ -3,11 +3,25 @@ module LoggerHelper
 
   def user_creds_log(username, password)
     log_message = "------------------------------------------------------ User creds: #{username}, #{password} ------------------------------------------------------"
-    p log_message
+    puts log_message
   end
 
   def last_request_log
+    end_line_log
     puts "Last request path: #{last_request.path_info}"
+    end_line_log
+  end
+
+  def last_request_method
+    end_line_log
+    puts "Last request method #{last_request.env['REQUEST_METHOD']}"
+    end_line_log
+  end
+
+  def last_response_status
+    end_line_log
+    puts "last response status code #{last_response.status}"
+    end_line_log
   end
 
   def last_response_log
@@ -24,18 +38,18 @@ module LoggerHelper
 
   def log_new_record_info(first_name, second_name, city, dob)
     log_message = "------------------------------------------------------ New record: #{first_name}, #{second_name}, #{city}, #{dob}!! ------------------------------------------------------"
-    p log_message
+    puts log_message
   end
 
   private
 
   def last_response_start_log_line
     log_message = '------------------------------------------------------ Last response ------------------------------------------------------------'
-    p log_message
+    puts log_message
   end
 
   def end_line_log
     log_message = '-----------------------------------------------------------------------------------------------------------------------------------------------'
-    p log_message
+    puts log_message
   end
 end
