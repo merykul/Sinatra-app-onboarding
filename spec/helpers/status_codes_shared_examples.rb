5b9e15d9fadc958a5a787981aa61a528eb549435
Mariia Kulyk <mariakulv@gmail.com>
2023-08-25 12:56:55 +0300

    added shared example 'get request with invalid data in the request url'

--- spec/helpers/status_codes_shared_examples.rb
+++ spec/helpers/status_codes_shared_examples.rb
@@ -5,6 +5,7 @@ data = YAML.load_file('data.yml')
 RSpec.shared_examples 'not authorised get' do |endpoint|
 
   let(:not_authorised_error) { data['not-authorised-error'] }
+  let(:page_not_found_error) { data['page-not-found-error'] }
 
   it 'verifies that response status code is 401' do
     get endpoint
@@ -26,5 +27,18 @@ RSpec.shared_examples 'authorised get request' do |endpoint|
 
   it 'user is redirected to requested page' do
     expect(last_response.body).to include(title)
+  end
+end
+
+RSpec.shared_examples 'get request with invalid data in the request url' do |endpoint|
+
+  it 'verifies that response code is 404' do
+    get endpoint
+    expect(last_response.status).to eq 404
+  end
+
+  it 'user is on "Invalid parameters" error page' do
+    get endpoint
+    expect(last_response.body).to include(page_not_found_error)
   end
 end
